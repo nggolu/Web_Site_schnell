2500a014f42825fc9d4a8b26d87f018dad4dd969
nishant02 <nishant.garg@schnll.com>
2017-03-27 18:53:46 +0530

    adding all files related to website

---
+++ WebRoot/js/map.js
@@ -0,0 +1,20 @@
+var myCenter=new google.maps.LatLng(28.45854, 77.04406);
+
+function initialize()
+{
+var mapProp = {
+  center:myCenter,
+  zoom:5,
+  mapTypeId:google.maps.MapTypeId.ROADMAP
+  };
+
+var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+
+var marker=new google.maps.Marker({
+  position:myCenter,
+  });
+
+marker.setMap(map);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
